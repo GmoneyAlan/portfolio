ead37a1c7300fe2a5c4959dac2508fb386c0ab0e
Alan Garcia <alan200@outlook.com>
2020-08-06 19:54:21 -0600

    service created to change pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import {ResumeComponent} from './resume/resume.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+//Added routes to route sites
+const routes: Routes = [
+  {path: 'resume-component', component: ResumeComponent},
+  {path: 'app-component', component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
